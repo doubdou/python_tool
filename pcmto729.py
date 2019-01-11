aa584558f54c771913be19bba546633778cdae6a
jasonlincoln <jasonlincoln@126.com>
2019-01-11 16:58:22 +0800

    完成调用解码器的操作。附上exe文件和两个示例音频

--- pcmto729.py
+++ pcmto729.py
@@ -3,17 +3,24 @@
 import os
 import re
 
-fileList = []
+main = "cp_g729_encoder.exe %s %s"
 
 def get_dir(path):
     for dirname, _, files in os.walk(path):
         for file in files:
             abs_name = os.path.join(dirname, file)
-            if re.match('(.*).txt', abs_name):
+            if abs_name.endswith("txt"):
+            #if re.match('(.*).txt', abs_name):
                 continue
             print(abs_name)
+            if abs_name.endswith("pcm"):
+                print(abs_name[:-3]+'G729')
+                r_v = os.system(main % (abs_name, abs_name[:-3]+'G729'))
+                os.remove(abs_name)
 
 
 if __name__ == '__main__':
-    get_dir("E:\\network_channel")
+    #get_dir("E:\\test")
+    get_dir("E:\\3")
+
 
